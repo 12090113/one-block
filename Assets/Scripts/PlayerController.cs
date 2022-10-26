e956d68c2231cf1cc7864b2649841939b7f2923a
12090113 <47487266+12090113@users.noreply.github.com>
2022-10-26 16:59:07 -0700

    Rename scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        LayerMask groundLayer = 1 << LayerMask.NameToLayer("Terrain");
     }
 
     // Update is called once per frame
