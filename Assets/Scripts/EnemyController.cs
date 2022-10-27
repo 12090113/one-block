ed82afeab92c1bad8d6d91dd8e3006c9eae51247
RCarScar <90071713+RCarScar@users.noreply.github.com>
2022-10-27 16:34:09 -0700

    SemiFunctionenemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,9 +9,12 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     GameObject player;
     [SerializeField]
-    float Health, damage = 10;
+    float Health, damage = 10, enemySpeed, enemyMaxVelocity, jumppower;
     [SerializeField]
     AIstate currentstate = AIstate.movingforward;
+    [SerializeField]
+    LayerMask groundLayer;
+    Rigidbody2D rb;
 
     enum AIstate{
         movingforward,jumping,idle
@@ -19,13 +22,18 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerMove = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Health <= 0 && gameObject != null)
+        if (Physics2D.OverlapCircle(transform.position + Vector3.down * .6f, .5f, groundLayer))
+        {
+            Jump();
+        }
+            if (Health <= 0 && gameObject != null)
         {
             Destroy(gameObject);
         }
@@ -34,11 +42,19 @@ public class EnemyController : MonoBehaviour
             case AIstate.movingforward:
                 if (player.transform.position.x > transform.position.x)
                 {
-
+                    rb.velocity = new Vector2(enemySpeed, rb.velocity.y);
                 }
                 if (player.transform.position.x < transform.position.x)
                 {
-
+                    rb.velocity = new Vector2(-enemySpeed, rb.velocity.y);
+                }
+                if (rb.velocity.x > enemyMaxVelocity)
+                {
+                    rb.velocity = new Vector2(enemyMaxVelocity, rb.velocity.y);
+                }
+                else if (rb.velocity.x < -enemyMaxVelocity)
+                {
+                    rb.velocity = new Vector2(-enemyMaxVelocity, rb.velocity.y);
                 }
                 break;
             case AIstate.jumping:
@@ -59,5 +75,17 @@ public class EnemyController : MonoBehaviour
             Health -= 0.5f * collision.rigidbody.mass * Mathf.Pow(collision.rigidbody.velocity.magnitude, 2);
             Debug.Log(collision.gameObject.name);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position + Vector3.right * .6f + Vector3.down * 0.5f, .1f);
+        Gizmos.DrawSphere(transform.position + Vector3.left * .6f + Vector3.down * 0.5f, .1f);
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumppower);
     }
 }
