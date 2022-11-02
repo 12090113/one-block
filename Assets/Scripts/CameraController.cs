49098ba6c403f853ed394ec7fecdea045b9d008f
12090113 <47487266+12090113@users.noreply.github.com>
2022-11-01 17:47:29 -0700

    Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform player;
+    private float speed = 0.125f;
+    public Vector3 offset = new Vector3(0, 0, -10);
+    public float scrollSpeed = 0.1f;
+    private Camera cam;
+    private float scroll;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        scroll = cam.orthographicSize;
+        player = FindObjectOfType<PlayerController>().transform;
+    }
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + offset, speed);
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y != 0f)
+        {
+            scroll += -cam.orthographicSize * Input.mouseScrollDelta.y * scrollSpeed;
+            scroll = Mathf.Clamp(scroll, 1, 10000);
+        }
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, scroll, 0.1f);
+    }
+}
