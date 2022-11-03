ecdc2641fd58fdaa43e7f4d064df44577d1dd695
RCarScar <90071713+RCarScar@users.noreply.github.com>
2022-11-03 16:11:18 -0700

    First

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,22 +5,5 @@ using UnityEngine.Tilemaps;
 
 public class Block : MonoBehaviour
 {
-    Rigidbody2D rb;
     public TileBase tile;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void FixedUpdate()
-    {
-        
-    }
-
 }
